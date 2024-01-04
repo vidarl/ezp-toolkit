57842c2d39e74f443c812453e5a9301d900bb275
Vidar Langseid <vl@ibexa.co>
2024-01-04 15:41:24 +0100

    fixup! Added docker setup for running xdebug

--- xdebug/internal_install_xdebug.sh
+++ xdebug/internal_install_xdebug.sh
@@ -2,25 +2,36 @@
 
 XDEBUG_VERSION=3.2.2
 
+option=$1
+forceReinstall=""
+
+if [[ "$option" = "--force-reinstall" ]]; then
+    forceReinstall="true"
+fi
 
 if [ ! -f /usr/local/etc/php/conf.d/99-xdebug.ini ]; then
-    echo "Installing XDEBUG"
-    rm -f /tmp/xdebug.tar.gz xdebug-${XDEBUG_VERSION}
-    curl https://xdebug.org/files/xdebug-${XDEBUG_VERSION}.tgz --output /tmp/xdebug.tar.gz
-    # apt-get install php-dev autoconf automake
-    apt-get update
-    apt-get install -y autoconf automake libtool iputils-ping telnet procps
+    if [ ! -f /usr/local/lib/php/extensions/no-debug-non-zts-20210902/xdebug.so ] || [ "$forceReinstall" = "true" ]; then
+        echo "(Re)Installing XDEBUG"
+        rm -f /tmp/xdebug.tar.gz xdebug-${XDEBUG_VERSION}
+        curl https://xdebug.org/files/xdebug-${XDEBUG_VERSION}.tgz --output /tmp/xdebug.tar.gz
+        # apt-get install php-dev autoconf automake
+        apt-get update
+        apt-get install -y autoconf automake libtool iputils-ping telnet procps
 
 
-    cd /tmp
-    tar -xzf xdebug.tar.gz
-    cd xdebug-${XDEBUG_VERSION}
-    phpize
-    # autoreconf -i
-    ./configure
-    make
+        cd /tmp
+        tar -xzf xdebug.tar.gz
+        cd xdebug-${XDEBUG_VERSION}
+        phpize
+        # autoreconf -i
+        ./configure
+        make
 
-    cp modules/xdebug.so /usr/local/lib/php/extensions/no-debug-non-zts-20210902
+        cp modules/xdebug.so /usr/local/lib/php/extensions/no-debug-non-zts-20210902
+
+        # Install validator scripts ( see Settings -> PHP -> Debug -> Validate debugger configuration on the Web Server -> Debug validaton script
+        sudo -u www-data bash -c "cd /var/www/public && curl -f -L  -o ./phpstorm_xdebug.zip "https://packages.jetbrains.team/files/p/ij/xdebug-validation-script/script/phpstorm_xdebug_validator.zip" && unzip -n ./phpstorm_xdebug.zip -d . && rm -f ./phpstorm_xdebug.zip"
+    fi
 
     echo "zend_extension = xdebug.so" > /usr/local/etc/php/conf.d/99-xdebug.ini
     #  echo "xdebug.mode=develop,coverage,debug,profile" >> /usr/local/etc/php/conf.d/99-xdebug.ini
@@ -32,9 +43,6 @@ if [ ! -f /usr/local/etc/php/conf.d/99-xdebug.ini ]; then
     echo "xdebug.client_port=$XDEBUG_PORT" >> /usr/local/etc/php/conf.d/99-xdebug.ini
     echo "xdebug.client_host=host.docker.internal" >> /usr/local/etc/php/conf.d/99-xdebug.ini
     echo "" >> /usr/local/etc/php/conf.d/99-xdebug.ini
-
-    # Install validator scripts ( see Settings -> PHP -> Debug -> Validate debugger configuration on the Web Server -> Debug validaton script
-    sudo -u www-data bash -c "cd /var/www/public && curl -f -L  -o ./phpstorm_xdebug.zip "https://packages.jetbrains.team/files/p/ij/xdebug-validation-script/script/phpstorm_xdebug_validator.zip" && unzip -n ./phpstorm_xdebug.zip -d . && rm -f ./phpstorm_xdebug.zip"
 
 else
     echo "XDEBUG already installed !"
--- xdebug/prepare_container_for_xdebug.sh
+++ xdebug/prepare_container_for_xdebug.sh
@@ -1,12 +1,37 @@
 #!/bin/bash
 
+# Usage :
+# ./external/ezp-toolkit/xdebug/prepare_container_for_xdebug.sh
+# ./external/ezp-toolkit/xdebug/prepare_container_for_xdebug.sh --disable
+# ./external/ezp-toolkit/xdebug/prepare_container_for_xdebug.sh --force-reinstall
+
+option=$1
+
+if [[ "$option" = "--disable" ]]; then
+    echo "Disabling xdebug by removing '/usr/local/etc/php/conf.d/99-xdebug.ini' inside container"
+    docker-compose exec app bash -c "rm -rf /usr/local/etc/php/conf.d/99-xdebug.ini"
+    docker-compose stop app
+    docker-compose up -d
+
+    exit
+fi
+
+
+forceReinstall=""
+if [[ "$option" = "--force-reinstall" ]]; then
+    forceReinstall="--force-reinstall"
+elif [[ "$option" != "" ]]; then
+    echo "Unkown option : $1"
+    exit 1
+fi
+
 # Insert wsl host's IP in .env.
 cat .env|grep "DOCKERHOST=" || echo -e "\nDOCKERHOST=1.2.3.4" >> .env
 sed -ie 's/DOCKERHOST=.*/DOCKERHOST='$(ip route show | awk '/default/ {print $3}')'/g' .env
 # If ip changed, app container will be recreated now:
 docker-compose up -d
 
-docker-compose exec app bash -c ./external/ezp-toolkit/xdebug/internal_install_xdebug.sh
+docker-compose exec app bash -c "./external/ezp-toolkit/xdebug/internal_install_xdebug.sh $forceReinstall"
 
 docker-compose stop app
 docker-compose up -d
